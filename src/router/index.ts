0107cf189fa84cc7191e8278e34dd3a1d97e28f5
yuansuixin <cyss428@163.com>
2021-02-26 11:04:10 +0800

    feat

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,25 +1,48 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/Home.vue'
+import Add from '@/components/Add.vue'
+import Delete from '@/components/Delete.vue'
+import Done from '@/components/Done.vue'
+import Edit from '@/components/Edit.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: Home,
+    children: [
+      {
+        path: '',
+        redirect: { name: 'add' }
+      },
+      {
+        path: '/add',
+        name: 'add',
+        component: Add
+      },
+      {
+        path: '/delete',
+        name: 'delete',
+        component: Delete
+      },
+      {
+        path: '/done',
+        name: 'done',
+        component: Done
+      },
+      {
+        path: '/edit',
+        name: 'edit',
+        component: Edit
+      }
+    ]
   }
 ]
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes
+  routes,
+  linkExactActiveClass: 'active'
 })
 
 export default router
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container ">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand d-flex align-items-center" href="#">
+        <div class="logo mr-2"></div>
+        <span>ToDoList</span>
+      </a>
+
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <router-link class="nav-link" to="/add">待办事项</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/done">已完成</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/delete ">已删除</router-link>
+        </li>
+      </ul>
+    </nav>
+    <div class="container">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
 export default defineComponent({
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
 })
 </script>
+
+<style lang="scss" scoped>
+.logo {
+  background: url('~@/assets/logo.png') no-repeat center center;
+  width: 40px;
+  height: 40px;
+  background-size: contain;
+}
+</style>
